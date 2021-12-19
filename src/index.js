cd974a725539874fc75ed0f14e41f4e68f0f59c3
Ömer Genç <omergnc1@gmail.com>
2021-12-20 01:27:00 +0300

    normalize css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
+import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
